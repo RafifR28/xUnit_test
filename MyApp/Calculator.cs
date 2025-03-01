b4571bbf79bcd7b18443e20ba9e8bf16b40eefb4
Rafif Ramadan <66911524+RafifR28@users.noreply.github.com>
2025-03-01 11:20:35 +0700

    Unit Test

---
+++ MyApp/Calculator.cs
@@ -0,0 +1,11 @@
+// filepath: /g:/My Drive/elevAlte - Microsoft Learning/xUnit_test/MyApp/Calculator.cs
+namespace MyApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ TestProject1_elevAlte/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿// filepath: /g:/My Drive/elevAlte - Microsoft Learning/xUnit_test/TestProject1_elevAlte/UnitTest1.cs
+namespace TestProject1_elevAlte;
+
+using Xunit;
+using MyApp;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var calculator = new Calculator();
+        int a = 5;
+        int b = 3;
+
+        // Act
+        int result = calculator.Add(a, b);
+
+        // Assert
+        Assert.Equal(8, result);
+    }
+}
